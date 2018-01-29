297a00acd180ca4f8a79591c6ae1d2fdeea56ba6
Jaagup Kippar <jaagup@tlu.ee>
2018-01-29 10:50:29 +0200

    ilm

--- esmaspaev/01algus/Ilm.java
+++ esmaspaev/01algus/Ilm.java
@@ -19,7 +19,19 @@ public class Ilm{
     }
     public static void main(String[] arg){
         int temperatuur=-15;
+        int sademed=0;
+        //Kui temperatuur [ja sademed] sisestatakse käsurealt,
+        //siis arvestatakse neid väärtusi
+        try{
+          if(arg.length>0){temperatuur=Integer.parseInt(arg[0]);}
+          if(arg.length>1){sademed=Integer.parseInt(arg[1]);}
+        } catch(Exception e){
+            System.out.println(
+             "Kasuta kujul java Ilm [temperatuur] [sademed], nt ");
+            System.out.println("java Ilm -15 2"); 
+            return;            
+        }
         System.out.println(kooliminekuHinnang(temperatuur));
-        System.out.println(riietuseHinnang(temperatuur, 2));
+        System.out.println(riietuseHinnang(temperatuur, sademed));
     }
 }
