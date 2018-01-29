d84ead08504d3bc86a91a9f06020a5356f552d15
Jaagup Kippar <jaagup@tlu.ee>
2018-01-29 09:19:26 +0200

    algus

--- esmaspaev/01algus/Tervitus.java
+++ esmaspaev/01algus/Tervitus.java
@@ -1,5 +1,17 @@
 public class Tervitus{
     public static void main(String[] arg){
         System.out.println("Tere");
+        int temperatuur=25;
+        if(temperatuur<-28){
+            System.out.println("Külmapüha");
+        } else {
+            //Kui õues on soojem kui 20 kraadi
+            //siis soovita lühikesi varrukaid
+            if(temperatuur>20){
+                System.out.println(
+                  "Ettevaatust, vahetunnis veesõda!");
+            }
+            System.out.println("Hommikul kooli");
+        }
     }
 }
