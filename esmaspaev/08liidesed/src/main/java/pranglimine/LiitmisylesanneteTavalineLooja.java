7026c8ee367f9376e3c7db884d73d8a8fcd83d40
Jaagup Kippar <jaagup@tlu.ee>
2018-03-26 11:18:50 +0300

    testid

---
+++ esmaspaev/08liidesed/src/main/java/pranglimine/LiitmisylesanneteTavalineLooja.java
@@ -0,0 +1,11 @@
+package pranglimine;
+public class LiitmisylesanneteTavalineLooja implements LiitmisylesanneteLoojaLiides{
+     @Override
+     public Liitmisylesanne looLiitmisylesanne(int maxsumma){
+         JuhuarvudeGeneraatoriLiides generaator=null;
+         int a1=generaator.looJuhuarv(5, 10);
+         int a2=generaator.looJuhuarv(0, 5);
+         int a3=a1-a2;
+         return new Liitmisylesanne(a3, a2);
+     }
+}
